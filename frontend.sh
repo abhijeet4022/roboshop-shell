157a81441238ead71fa6165154b39eabd6503192
abhijeet4022 <abhijeet4022@gmail.com>
2023-10-05 11:05:40 +0530

    Roboshop Project Script

--- frontend.sh
+++ frontend.sh
@@ -1,12 +1,13 @@
 #hostnamectl set-hostname frontend
 #sleep 5
+cp nginx-proxy.conf /etc/nginx/default.d/nginx-proxy.conf
 yum install -y nginx bash-completion
 systemctl start nginx
 systemctl enable nginx
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 unzip -o /tmp/frontend.zip -d /usr/share/nginx/html/
-cp nginx-proxy.conf /etc/nginx/default.d/nginx-proxy.conf
+
 systemctl restart nginx
 
 # Repo URL "https://github.com/abhijeet4022/roboshop-shell.git"
