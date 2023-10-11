caa2c611237fee98a676ad7961613633f8abd6ea
abhijeet4022 <abhijeet4022@gmail.com>
2023-10-11 10:26:30 +0530

    Conditions

--- common.sh
+++ common.sh
@@ -1,35 +1,49 @@
 # Log Variable.
     log=/tmp/roboshop.log
-
+func_exit_status(){
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m Success \e[0m"
+  else
+    echo -e "\e[31m Failure \e[0m"
+  fi
+}
 
 func_appprerequisite(){
     #We need to setup a new service in systemd so systemctl can manage this service
     # Setup SystemD Shipping Service
     echo -e "\e[34m-->> Deleting The old service /etc/systemd/system/$component.service file.\e[0m" | tee -a ${log}
     rm -rf /etc/systemd/system/$component.service  &>> ${log}
+    func_exit_status
 
     echo -e "\e[34m-->> Copying The $component.service to /etc/systemd/system/$component.service.\e[0m" | tee -a ${log}
     cp $component.service /etc/systemd/system/$component.service  &>> ${log}
+    func_exit_status
 
     # Add application user
     echo -e "\e[34m-->> Creating Application User- roboshop.\e[0m" | tee -a ${log}
     useradd roboshop  &>> ${log}
+    func_exit_status
 
     # Setup an app directory.
     echo -e "\e[34m-->> Removing the existing application directory.\e[0m" | tee -a ${log}
     rm -rf /app &>> ${log}
+    func_exit_status
 
     echo -e "\e[34m-->> Creating Application Directory- /app.\e[0m" | tee -a ${log}
     mkdir /app  &>> ${log}
+    func_exit_status
 
     # Download the application code to created app directory.
     echo -e "\e[34m-->> Downloading the Application code.\e[0m" | tee -a ${log}
     curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>> ${log}
+    func_exit_status
 
     echo -e "\e[34m-->> Extracting the Application code.\e[0m" | tee -a ${log}
     unzip -o /tmp/$component.zip -d /app/ &>> ${log}
+    func_exit_status
 
     echo -e "\e[35m**** func_appprerequisite Function Completed****\e[0m" | tee -a ${log}
+    func_exit_status
 }
 
 
@@ -39,10 +53,12 @@ func_systemd() {
     #This command is because we added a new service, We are telling systemd to reload so it will detect new service.
     echo -e "\e[34m-->> Starting & Enabling the $component service.\e[0m" | tee -a ${log}
     systemctl daemon-reload  &>> ${log}
+    func_exit_status
 
     # Start & Enable the service.
     systemctl enable $component  &>> ${log}
     systemctl restart $component  &>> ${log}
+    func_exit_status
 
     echo -e "\e[35m**** func_systemd Function Completed****\e[0m" | tee -a ${log}
 }
@@ -58,13 +74,16 @@ func_schema_setup(){
 
   echo -e "\e[34m-->> Configuring the repo for mongodb.\e[0m" | tee -a ${log}
   cp mongo.repo /etc/yum.repos.d/mongo.repo  &>> ${log}
+  func_exit_status
 
   echo -e "\e[34m-->> Installing the mongodb-org-shell package.\e[0m" | tee -a ${log}
   dnf install mongodb-org-shell -y  &>> ${log}
+  func_exit_status
 
   #Load Schema
   echo -e "\e[34m-->> Loading the mongodb schema.\e[0m" | tee -a ${log}
   mongo --host mongodb.learntechnology.tech </app/schema/$component.js  &>> ${log}
+  func_exit_status
 
   fi
 
@@ -73,9 +92,11 @@ func_schema_setup(){
 
   echo -e "\e[34m-->> Installing the MYSQL client.\e[0m" | tee -a ${log}
   dnf install mysql -y   &>> ${log}
+  func_exit_status
 
   echo -e "\e[34m-->> Loading the schema.\e[0m" | tee -a ${log}
   mysql -h mysql.learntechnology.tech -uroot -pRoboShop@1 </app/schema/$component.sql   &>> ${log}
+  func_exit_status
 
   fi
 
@@ -94,23 +115,28 @@ func_nodejs(){
   # Configuring repo to download nodejs NodeJS>=18
   echo -e "\e[34m-->> Configuring the repo for NodeJS.\e[0m" | tee -a ${log}
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>> ${log}
+  func_exit_status
 
   echo -e "\e[34m-->> Installing NodeJS & bash-completion package.\e[0m" | tee -a ${log}
   yum install -y bash-completion nodejs  &>> ${log}
+  func_exit_status
 
   # Calling the function
   echo -e "\e[35m**** Calling  Calling the func_appprerequisite function.****\e[0m" | tee -a ${log}
   func_appprerequisite
+  func_exit_status
 
 
   # Every application is developed by development team will have some common software's that they use as libraries. This application also have the same way of defined dependencies in the application configuration.
   #Lets download the dependencies.
   echo -e "\e[34m-->> Installing the dependencies for the Application- /app/package.json.\e[0m" | tee -a ${log}
   npm install -C /app/  &>> ${log} # We can install dependency without navigate to that directory.
+  func_exit_status
 
   # Calling the function
   echo -e "\e[35m**** Calling  Calling the func_schema_setup function to setup mongodb.****\e[0m" | tee -a ${log}
   func_schema_setup
+  func_exit_status
 
   #Need to update catalogue server ip address in frontend configuration.
   # Configuration file is /etc/nginx/default.d/roboshop.conf
@@ -120,6 +146,7 @@ func_nodejs(){
   # Calling the function
   echo -e "\e[35m**** Calling the func_systemd function.****\e[0m" | tee -a ${log}
   func_systemd
+  func_exit_status
 
   echo -e "\e[33m-----------Script Run Successfully-----------\e[0m" | tee -a ${log}
 }
@@ -136,10 +163,12 @@ func_java(){
 
   echo -e "\e[34m-->> Installing the MAVAN package.\e[0m" | tee -a ${log}
   dnf install maven bash-completion -y  &>> ${log}
+  func_exit_status
 
   #Calling the function.
   echo -e "\e[35m**** Calling  Calling the func_appprerequisite function.****\e[0m" | tee -a ${log}
   func_appprerequisite
+  func_exit_status
 
   #Every application is developed by development team will have some common software's that they use as libraries.
   # This application also have the same way of defined dependencies in the application configuration.
@@ -147,14 +176,17 @@ func_java(){
   echo -e "\e[34m-->> Building the $component service.\e[0m" | tee -a ${log}
   mvn clean package -f /app/pom.xml  &>> ${log}
   mv /app/target/$component-1.0.jar /app/$component.jar  &>> ${log}
+  func_exit_status
 
   # Calling the function
   echo -e "\e[35m**** Calling  Calling the func_schema_setup function to setup MYSQL.****\e[0m" | tee -a ${log}
   func_schema_setup
+  func_exit_status
 
   # Calling the function
   echo -e "\e[35m**** Calling the func_systemd function.****\e[0m" | tee -a ${log}
   func_systemd
+  func_exit_status
 
   echo -e "\e[33m-----------Script Run Successfully-----------\e[0m" | tee -a ${log}
 }
@@ -168,10 +200,12 @@ func_python(){
   # Install Python 3.6
   echo -e "\e[34m-->> Installing the Python package.\e[0m" | tee -a ${log}
   dnf install python36 gcc python3-devel bash-completion -y   &>> ${log}
+  func_exit_status
 
   #Calling the function.
   echo -e "\e[35m**** Calling  Calling the func_appprerequisite function.****\e[0m" | tee -a ${log}
   func_appprerequisite
+  func_exit_status
 
   # Every application is developed by development team will have some common software's that they use as libraries.
   # This application also have the same way of defined dependencies in the application configuration.
@@ -179,9 +213,11 @@ func_python(){
 
   echo -e "\e[34m-->> Building the $component service.\e[0m" | tee -a ${log}
   pip3.6 install -r /app/requirements.txt &>> ${log}
+  func_exit_status
 
   echo -e "\e[35m**** Calling the func_systemd function.****\e[0m" | tee -a ${log}
   func_systemd
+  func_exit_status
 
   echo -e "\e[33m-----------Script Run Successfully-----------\e[0m" | tee -a ${log}
 }
@@ -194,23 +230,27 @@ func_golang(){
   #Install GoLang
   echo -e "\e[34m-->> Installing the golang package.\e[0m" | tee -a ${log}
   dnf install bash-completion golang -y  &>> ${log}
+  func_exit_status
 
   #Calling the function.
   echo -e "\e[35m**** Calling  Calling the func_appprerequisite function.****\e[0m" | tee -a ${log}
   func_appprerequisite
+  func_exit_status
 
   #Every application is developed by development team will have some common software's that they use as libraries.
   # This application also have the same way of defined dependencies in the application configuration.
   #Lets download the dependencies & build the software.
-
+  echo -e "\e[34m-->> Building the Application Package.\e[0m" | tee -a ${log}
   cd /app  &>> ${log} # From outside we can't run these command so need to navigate to that directory
   # if first command fail then it will exit || means or operator
   go mod init dispatch   &>> ${log} #dispatch is the module name we can change it but we have to modify in config file also
   go get  &>> ${log}
   go build  &>> ${log}
+  func_exit_status
 
   echo -e "\e[35m**** Calling the func_systemd function.****\e[0m" | tee -a ${log}
   func_systemd
+  func_exit_status
 
   echo -e "\e[33m-----------Script Run Successfully-----------\e[0m" | tee -a ${log}
 }
