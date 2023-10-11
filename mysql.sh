b8da2d42f51cf3f9ddfc91880a5537241258202f
abhijeet4022 <abhijeet4022@gmail.com>
2023-10-11 11:46:59 +0530

    Removing hardcoded password.

--- mysql.sh
+++ mysql.sh
@@ -1,8 +1,8 @@
-# Change the hostname
-#hostnamectl set-hostname mysql
-# Put  sleep for 5S so hostname fully propagate before running the next script.
-#sleep 5
-
+mysql_root_password=$1
+if [ -z "${mysql_root_password}" ]; then
+  echo -e "\e[31m Input Password is Missing.\e[0m"
+  exit 1
+fi
 # Developer has chosen the database MySQL. Hence, we are trying to install it up and configure it.
 # CentOS-8 Comes with MySQL 8 Version by default, However our application needs MySQL 5.7. So lets disable MySQL 8 version.
 dnf module disable mysql -y
@@ -18,6 +18,6 @@ systemctl enable mysqld
 systemctl restart mysqld
 
 # We need to change the default root password in order to start using the database service. Use password RoboShop@1 or any other as per your choice.
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass ${mysql_root_password}
 
 echo "-----------Script Run Successfully-----------"
