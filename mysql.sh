03c7aa231214af3925a68cb7b380aa8a6bbd1da9
abhijeet4022 <abhijeet4022@gmail.com>
2023-10-12 10:01:59 +0530

    Removing hardcoded password.

--- mysql.sh
+++ mysql.sh
@@ -8,29 +8,29 @@ source common.sh
 # Developer has chosen the database MySQL. Hence, we are trying to install it up and configure it.
 # CentOS-8 Comes with MySQL 8 Version by default, However our application needs MySQL 5.7. So lets disable MySQL 8 version.
 
-echo -e "\e[33m**** Disable the mysql default repo. ****\e[0m" | tee -a ${log}
+echo -e "\e[34m-->> Disable the mysql default repo. \e[0m" | tee -a ${log}
 dnf module disable mysql -y  &>> ${log}
 func_exit_status
 
 # Setup the MySQL5.7 repo file
-echo -e "\e[33m**** Configuring mysql5.7 repository repo. ****\e[0m" | tee -a ${log}
+echo -e "\e[34m-->> Configuring mysql5.7 repository repo. \e[0m" | tee -a ${log}
 cp mysql.repo /etc/yum.repos.d/mysql.repo    &>> ${log}
 func_exit_status
 
 #Install MySQL Server
-echo -e "\e[33m**** Installing the mysql-community-server package. ****\e[0m" | tee -a ${log}
+echo -e "\e[34m-->> Installing the mysql-community-server package. \e[0m" | tee -a ${log}
 dnf install mysql-community-server bash-completion -y     &>> ${log}
 func_exit_status
 
 # Start MySQL Service
-echo -e "\e[33m**** Starting & Enabling the mysqld service. ****\e[0m" | tee -a ${log}
+echo -e "\e[34m-->> Starting & Enabling the mysqld service. \e[0m" | tee -a ${log}
 systemctl enable mysqld    &>> ${log}
 systemctl restart mysqld     &>> ${log}
 func_exit_status
 
 # We need to change the default root password in order to start using the database service. Use password RoboShop@1 or any other as per your choice.
 #Password = RoboShop@1
-echo -e "\e[33m**** Configuring the MYSQl root password. ****\e[0m" | tee -a ${log}
+echo -e "\e[34m-->> Configuring the MYSQl root password. \e[0m" | tee -a ${log}
 mysql_secure_installation --set-root-pass ${mysql_root_password}     &>> ${log}
 func_exit_status
 
--- rabbitmq.sh
+++ rabbitmq.sh
@@ -8,29 +8,29 @@ source common.sh
 
 
 # Configure YUM Repos from the script provided by vendor. this is dependency of rabbitmq
-echo -e "\e[33m**** Configuring the erlang repository. ****\e[0m" | tee -a ${log}
+echo -e "\e[34m-->> Configuring the erlang repository. \e[0m" | tee -a ${log}
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash  &>> ${log}
 func_exit_status
 
 # Configure YUM Repos for RabbitMQ.
-echo -e "\e[33m**** Configuring the RabbitMQ repository. ****\e[0m" | tee -a ${log}
+echo -e "\e[34m-->> Configuring the RabbitMQ repository. \e[0m" | tee -a ${log}
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash  &>> ${log}
 func_exit_status
 
 # Install RabbitMQ
-echo -e "\e[33m**** Installing the rabbitmq-server package. ****\e[0m" | tee -a ${log}
+echo -e "\e[34m-->> Installing the rabbitmq-server package. \e[0m" | tee -a ${log}
 dnf install rabbitmq-server bash-completion -y  &>> ${log}
 func_exit_status
 
 # Start RabbitMQ Service
-echo -e "\e[33m**** Starting & Enabling the rabbitmq-server service . ****\e[0m" | tee -a ${log}
+echo -e "\e[34m-->> Starting & Enabling the rabbitmq-server service . \e[0m" | tee -a ${log}
 systemctl enable rabbitmq-server  &>> ${log}
 systemctl restart rabbitmq-server  &>> ${log}
 func_exit_status
 
 # RabbitMQ comes with a default username / password as guest/guest. But this user cannot be used to connect.
 # Hence, we need to create one user for the application.
-echo -e "\e[33m**** Configuring the RabbitMQ User/Pass . ****\e[0m" | tee -a ${log}
+echo -e "\e[34m-->> Configuring the RabbitMQ User/Pass . \e[0m" | tee -a ${log}
 rabbitmqctl add_user roboshop ${rabbitmq_appuser_password}  &>> ${log}
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"   &>> ${log}
 func_exit_status
